9cabddfd9efe8a0f4524f1b1295ff5308228cd32
uczen <uczen@email.pl>
2024-03-12 13:31:28 +0100

    Add add functions to manage content of list

--- ListaProduktow/ListaProduktow/MainPage.xaml.cs
+++ ListaProduktow/ListaProduktow/MainPage.xaml.cs
@@ -13,9 +13,48 @@ namespace ListaProduktow
     {
         private Produkt wybranyProdukt;
         public ObservableCollection<Produkt> produkty = new ObservableCollection<Produkt>();
+
+        public void AktualizujListe()
+        {
+            lista.ItemsSource = null;
+            lista.ItemsSource = produkty;
+        }
         public MainPage()
         {
             InitializeComponent();
+            OnAppearing();
+        }
+
+        public void Dodaj_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ManageProduct(produkty));
+        }
+        public void Edytuj_Clicked(object sender, EventArgs e)
+        {
+            if (lista.SelectedItem != null)
+            {
+                wybranyProdukt = (Produkt)lista.SelectedItem;
+                Navigation.PushAsync(new ManageProduct(wybranyProdukt));
+                wybranyProdukt = null;
+            }
+            else
+            {
+                DisplayAlert("Błąd", "Nie wybrano elementu", "OK");
+            }
+        }
+        public void Usun_Clicked(object sender, EventArgs e)
+        {
+            if (lista.SelectedItem != null)
+            {
+                wybranyProdukt = (Produkt)lista.SelectedItem;
+                produkty.Remove(wybranyProdukt);
+                AktualizujListe();
+                wybranyProdukt = null;
+            }
+            else
+            {
+                DisplayAlert("Błąd", "Nie wybrano elementu", "OK");
+            }
         }
     }
 }
