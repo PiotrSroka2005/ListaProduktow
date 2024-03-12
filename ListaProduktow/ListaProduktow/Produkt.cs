393f84e7482cad989ef921da88efca27d0a0beee
uczen <uczen@email.pl>
2024-03-12 13:27:48 +0100

    Add produkt class

---
+++ ListaProduktow/ListaProduktow/Produkt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListaProduktow
+{
+    public class Produkt
+    {
+        public string Nazwa { get; set; }
+        public decimal Cena { get; set; }
+        public int Ilosc { get; set; }
+
+        public Produkt(string nazwa, decimal cena, int ilosc)
+        {
+            Nazwa = nazwa;
+            Cena = cena;
+            Ilosc = ilosc;
+        }
+        public Produkt() { }
+    }
+}
