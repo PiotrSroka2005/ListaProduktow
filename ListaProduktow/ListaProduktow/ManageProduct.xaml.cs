d1835e98919c001e540f7c0eab35481f94c8212e
uczen <uczen@email.pl>
2024-03-12 13:48:48 +0100

    Add xaml cs in ManageProduct page

--- ListaProduktow/ListaProduktow/ManageProduct.xaml.cs
+++ ListaProduktow/ListaProduktow/ManageProduct.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,69 @@ using Xamarin.Forms.Xaml;
 namespace ListaProduktow
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ManageProdukt : ContentPage
+    public partial class ManageProduct : ContentPage
     {
-        public ManageProdukt()
+        private Produkt wybranyProdukt;
+        private ObservableCollection<Produkt> produkty = new ObservableCollection<Produkt>();
+        public ManageProduct(ObservableCollection<Produkt> produkty)
         {
             InitializeComponent();
+            this.produkty = produkty;
+        }
+
+        public ManageProduct(Produkt wybranyProdukt)
+        {
+            InitializeComponent();
+            this.wybranyProdukt = wybranyProdukt;
+            labelTytul.Text = "Edytuj produkt";
+            entryNazwa.Text = wybranyProdukt.Nazwa;
+            entryCena.Text = wybranyProdukt.Cena.ToString();
+            entryIlosc.Text = wybranyProdukt.Ilosc.ToString();
+            btnDodaj.IsVisible = false;
+            btnEdytuj.IsVisible = true;
+        }
+
+        private void BtnDodajClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(entryNazwa.Text) && !string.IsNullOrEmpty(entryCena.Text) && !string.IsNullOrEmpty(entryIlosc.Text))
+            {
+                if (int.TryParse(entryIlosc.Text, out int _) && decimal.TryParse(entryCena.Text, out decimal _))
+                {
+                    var produkt = new Produkt(entryNazwa.Text, decimal.Parse(entryCena.Text), int.Parse(entryIlosc.Text));
+                    produkty.Add(produkt);
+                    Navigation.PopAsync();
+                }
+                else
+                {
+                    DisplayAlert("Niepoprawne dane", "Pola cena i ilość muszą być liczbami", "OK");
+                }
+            }
+            else
+            {
+                DisplayAlert("Niepoprawne dane", "Pola nie mogą byc puste", "OK");
+            }
+        }
+
+        private void BtnEdytujClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(entryNazwa.Text) && !string.IsNullOrEmpty(entryCena.Text) && !string.IsNullOrEmpty(entryIlosc.Text))
+            {
+                if (int.TryParse(entryIlosc.Text, out int _) && decimal.TryParse(entryCena.Text, out decimal _))
+                {
+                    wybranyProdukt.Nazwa = entryNazwa.Text;
+                    wybranyProdukt.Cena = decimal.Parse(entryCena.Text);
+                    wybranyProdukt.Ilosc = int.Parse(entryIlosc.Text);
+                    Navigation.PopAsync();
+                }
+                else
+                {
+                    DisplayAlert("Niepoprawne dane", "Pola cena i ilość muszą być liczbami", "OK");
+                }
+            }
+            else
+            {
+                DisplayAlert("Niepoprawne dane", "Pola nie mogą byc puste", "OK");
+            }
         }
     }
 }
---
+++ ListaProduktow/ListaProduktow/Plik.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ListaProduktow
+{
+    public class Plik
+    {
+
+        readonly static string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "products.txt");
+
+
+        public static List<Produkt> ReadData()
+        {
+            if (File.Exists(filePath))
+            {
+                List<string> lines = File.ReadAllLines(filePath).ToList();
+                List<Produkt> produkty = new List<Produkt>();
+
+                foreach (string line in lines)
+                {
+                    string[] entries = line.Split(';');
+                    
+                    Produkt produkt = new Produkt();
+
+                    produkt.Nazwa = entries[0];
+                    produkt.Cena = int.Parse(entries[1]);
+                    produkt.Ilosc = int.Parse(entries[2]);
+
+                    produkty.Add(produkt);
+                }
+                return produkty;
+            }    
+            else
+            {
+                return null;
+            }
+        }
+
+
+        public static void WriteToFile(List<Produkt> produkty)
+        {
+            List<string> outputFile = new List<string>();
+
+            foreach(var produkt in produkty)
+            {
+                string line = $"{produkt.Nazwa};{produkt.Ilosc};{produkt.Cena}";
+                outputFile.Add(line);
+            }
+            File.WriteAllLines(filePath, outputFile);
+        }
+
+        public static void RemoveFromFile()
+        {
+
+        }
+    }
+}
